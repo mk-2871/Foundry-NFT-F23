4d31e237bf44cde5e0aa6a051aada367dfde828a
S.MOHANAKUMAR <96325451+mk-2871@users.noreply.github.com>
2025-06-19 23:07:24 +0530

    Added MoodNft file

---
+++ src/MoodNft.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {ERC721} from "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+
+contract MoodNft is ERC721 {
+    
+}
