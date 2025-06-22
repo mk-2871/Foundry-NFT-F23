c08e0a0755946c08344e40f551df73ac5b889fd0
S.MOHANAKUMAR <96325451+mk-2871@users.noreply.github.com>
2025-06-22 22:36:19 +0530

    Formated spacing to avoid git foundry check

--- test/DeployMoodNft.t.sol
+++ test/DeployMoodNft.t.sol
@@ -15,17 +15,12 @@ contract DeployMoodNftTest is Test {
     }
 
     function testConvertSvgToURI() public view {
-        string
-            memory expectedUri = "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cHM6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iNTAwIiBoZWlnaHQ9IjUwMCI+PHRleHQgeD0iMCIgeT0iMTUiIGZpbGw9ImJsYWNrIj5IaSEgWW91ciBicm93c2VyIGRlY29kZWQgdGhpczwvdGV4dD48L3N2Zz4=";
-        string
-            memory svg = '<svg xmlns="https://www.w3.org/2000/svg" width="500" height="500"><text x="0" y="15" fill="black">Hi! Your browser decoded this</text></svg>';
-        string memory svgBase64Encoded = Base64.encode(
-            bytes(string(abi.encodePacked(svg)))
-        );
+        string memory expectedUri =
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cHM6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iNTAwIiBoZWlnaHQ9IjUwMCI+PHRleHQgeD0iMCIgeT0iMTUiIGZpbGw9ImJsYWNrIj5IaSEgWW91ciBicm93c2VyIGRlY29kZWQgdGhpczwvdGV4dD48L3N2Zz4=";
+        string memory svg =
+            '<svg xmlns="https://www.w3.org/2000/svg" width="500" height="500"><text x="0" y="15" fill="black">Hi! Your browser decoded this</text></svg>';
+        string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))));
         string memory actualUri = deployer.svgToImageURI(svg);
-        assert(
-            keccak256(abi.encodePacked(actualUri)) ==
-                keccak256(abi.encodePacked(expectedUri))
-        );
+        assert(keccak256(abi.encodePacked(actualUri)) == keccak256(abi.encodePacked(expectedUri)));
     }
 }
