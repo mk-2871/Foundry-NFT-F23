6a8c68a00a36789fc669bb2c9485f3fb7c86990a
S.MOHANAKUMAR <96325451+mk-2871@users.noreply.github.com>
2025-06-26 23:05:09 +0530

    added comments and refactored

--- script/DeployMoodNft.s.sol
+++ script/DeployMoodNft.s.sol
@@ -21,6 +21,7 @@ contract DeployMoodNft is Script {
         string memory baseURL = "data:image/svg+xml;base64,";
         string memory svgBase64Encoded = Base64.encode((bytes(string(abi.encodePacked(svg)))));
         return string(abi.encodePacked(baseURL, svgBase64Encoded));
+        //Checks if the SVG is valid
         
     }
 }
